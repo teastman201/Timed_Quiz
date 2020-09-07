4f69bd7936aadd361226bdcda10bde01a1e3edf2
Tristan Eastman <teastman85@gmail.com>
2020-09-07 10:40:25 -0400

    testing click function.

--- Assets/script.js
+++ Assets/script.js
@@ -54,18 +54,17 @@ function checkAns() {
     if (randomQuestion == myArray[0][0]) {
         for (i = 1; i < myArray[0].length; i++) {
             $("#ans" + i).text(myArray[0][i]);
+        }        
+        if ($("#ans1")[0].outerText == myArray[0][1]) {
+            // console.log(myArray[0][1]);
         }
-        
-        // if ($("#ans1")[0].outerText == myArray[0][1]) {
-        //     console.log(myArray[0][1]);
-        // }
     }
     if (randomQuestion == myArray[1][0]) {
         for (i = 1; i < myArray[1].length; i++) {
             $("#ans" + i).text(myArray[1][i]);
         }
         if ($("#ans3")[0].outerText == myArray[1][3]) {
-            console.log(myArray[1][3]);
+            // console.log(myArray[1][3]);
         }
     }
     if (randomQuestion == myArray[2][0]) {
@@ -73,7 +72,7 @@ function checkAns() {
             $("#ans" + i).text(myArray[2][i]);
         }
         if ($("#ans4")[0].outerText == myArray[2][4]) {
-            console.log(myArray[2][4]);
+            // console.log(myArray[2][4]);
         }
     }
     if (randomQuestion == myArray[3][0]) {
@@ -81,7 +80,7 @@ function checkAns() {
             $("#ans" + i).text(myArray[3][i]);
         }
         if ($("#ans2")[0].outerText == myArray[3][2]) {
-            console.log(myArray[3][2]);
+            // console.log(myArray[3][2]);
         }
     }
     if (randomQuestion == myArray[4][0]) {
@@ -89,7 +88,7 @@ function checkAns() {
             $("#ans" + i).text(myArray[4][i]);
         }
         if ($("#ans4")[0].outerText == myArray[4][4]) {
-            console.log(myArray[4][4]);
+            // console.log(myArray[4][4]);
         }
     }
 
@@ -116,9 +115,23 @@ function timerStart() {
 }
 
 $(this).click(function(){
-    if ($(this)[0].outerText == myArray[0][1]) {
+    if ($("#ans1")[0].outerText == myArray[0][1]) {
         console.log(myArray[0][1]);
-    } 
+
+    } else if ($("#ans3")[0].outerText == myArray[1][3]) {
+            console.log(myArray[1][3]);
+    
+    } else if ($("#ans4")[0].outerText == myArray[2][4]) {       
+            console.log(myArray[2][4]);
+
+    } else if ($("#ans2")[0].outerText == myArray[3][2]) {
+        console.log(myArray[3][2]);
+
+    } else if ($("#ans4")[0].outerText == myArray[4][4]) {
+        console.log(myArray[4][4]);
+    } else {
+        console.log('none of these are right.')
+    }
     
   });
 
